24801ef8702c2e9093aac47156c13d30b85f1829
Gurram Tharun Kumar <gurram.tharun.kumar@ibm.com>
2021-07-28 14:19:01 +0530

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { CreateUserComponent } from './create-user/create-user.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { UpdateUserComponent } from './update-user/update-user.component';
+import {FormsModule} from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserListComponent,
+    CreateUserComponent,
+    UserDetailsComponent,
+    UpdateUserComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
